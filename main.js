1b8b3e41b19d6bfe3b46cb69f54af3ca9766671e
yara <yarah8215@gmail.com>
2024-10-29 05:02:47 +0300

    Add landing page project with HTML, CSS, JS, and images folder

---
+++ main.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.img');//img element
+function phones(phone){
+    img.src=phone;
+}
+let container=document.querySelector('.container');
+function colors(color){
+    container.style.background=color;
+}
